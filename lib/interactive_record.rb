3c72da2f0fb15fb1120001f4fa81887fae06269e
Hazel Anne Villareal <hae.villareal@gmail.com>
2019-08-27 20:41:01 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -55,8 +55,7 @@ class InteractiveRecord
   def self.find_by(attribute)
     attribute.each do |key, value|
         sql = "SELECT * FROM #{table_name} WHERE #{key.to_s} = ?"
-        # binding.pry
-    return DB[:conn].execute(sql, value.to_s)
+    DB[:conn].execute(sql, value.to_s)
     end
   end
 end
